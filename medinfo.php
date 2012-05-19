c65d3e84d347e7f89e934172e8154456c8fd521d
David Tan <chengtan@live.com>
2012-05-19 15:06:57 -0400

    retrieved the rxcui from the rxnorm RESTful API

--- medinfo.php
+++ medinfo.php
@@ -1,5 +1,23 @@
 <?php
-	$xml_parser = xml_parser_create();
 	
+	$url='http://rxnav.nlm.nih.gov/REST/rxcui?name=lipitor';
+	$xml = simplexml_load_file($url);
+	$mname=$xml->idGroup[0]->name;
+	$drxcui=$xml->idGroup[0]->rxnormId;
+	echo $mname;
+	if($drxcui){
+		
+		echo $drxcui;
+	}else{
+		echo 'no id';
+	}
+	
+	
+	/*
+	$url='http://localhost/SRHPWebService/';
+	$xml = simplexml_load_file($url);
+	$lat=$xml->facility[0]->latitude;
+	echo $lat;
+	*/
 	
 ?>
