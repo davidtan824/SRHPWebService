f486446186be859d12a195b234eecaad667d8084
David Tan <chengtan@live.com>
2012-05-21 01:39:00 -0400

    linked the drug name to the medlineplus page, need to parse that html next

--- medinfo.php
+++ medinfo.php
@@ -2,22 +2,24 @@
 	
 	$url='http://rxnav.nlm.nih.gov/REST/rxcui?name=lipitor';
 	$xml = simplexml_load_file($url);
+	//var_dump($xml);
 	$mname=$xml->idGroup[0]->name;
 	$drxcui=$xml->idGroup[0]->rxnormId;
-	echo $mname;
+	//echo $mname;
 	if($drxcui){
 		
-		echo $drxcui;
+		//echo $drxcui;
 	}else{
 		echo 'no id';
 	}
+	$medlineplusurl='http://apps2.nlm.nih.gov/medlineplus/services/mpconnect_service.cfm?mainSearchCriteria.v.cs=2.16.840.1.113883.6.88&mainSearchCriteria.v.c='.$drxcui;
 	
+	$medlinexml=simplexml_load_file($medlineplusurl);
 	
-	/*
-	$url='http://localhost/SRHPWebService/';
-	$xml = simplexml_load_file($url);
-	$lat=$xml->facility[0]->latitude;
-	echo $lat;
-	*/
+	$link=$medlinexml->entry->link->attributes()->href;
+	
+	$html=file_get_contents($link);
+	echo $html;
 	
 ?>
+
