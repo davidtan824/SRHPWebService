61337d01fbb3dfd58b7e3496e3cfe69ce26185a6
David Tan <chengtan@live.com>
2012-05-22 04:05:05 +0800

    successfully parsed the html, used a recursive function to traverse the html dom and get all related information, need to format it or convert it into xml if needed

--- medinfo.php
+++ medinfo.php
@@ -6,22 +6,50 @@
 	
 	$mname=$xml->idGroup[0]->name;
 	$drxcui=$xml->idGroup[0]->rxnormId;
-	
-	if($drxcui){
-		
-		//echo $drxcui;
-	}else{
-		echo 'no id';
-	}
+	echo $drxcui.'<br/>';
+
 	$medlineplusurl='http://apps2.nlm.nih.gov/medlineplus/services/mpconnect_service.cfm?mainSearchCriteria.v.cs=2.16.840.1.113883.6.88&mainSearchCriteria.v.c='.$drxcui;
 	
 	$medlinexml=simplexml_load_file($medlineplusurl);
 	
 	$link=$medlinexml->entry->link->attributes()->href;
 	$html=file_get_html($link);
-	foreach($html->find('.drugResults dt a') as $e) 
 	
-	echo '<a href="'.$e->href.'">'.$e->innertext.'</a>';
+	foreach($html->find('.drugResults dt a') as $e) 
+	{
+		$detailurl=$e->href;
+	
+		$detailhtml=file_get_html($detailurl);
+		
+		$titles=$detailhtml->find('h2');
+		
+		foreach($titles as $title){
+			if($title->innertext!='Notice:'){
+			echo '<b>'.$title->innertext.'</b><br/>';
+			$titlediv=$title->parent();
+			
+			echo getblurb($titlediv);
+			}
+		}
+		
+	}
+	
+	function getblurb($e){
+		$blurb="";
+		if(($e->next_sibling()->tag=='p')||($e->next_sibling()->tag=='h3')){
+			
+			$blurb.=$e->next_sibling()->innertext.'<br/>';
+			
+			getblurb($e->next_sibling());
+			
+			return $blurb;
+		}else{
+			return;
+		}
+		
+		
+	}
+	
     
 		
 	
