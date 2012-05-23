4fcf11112fbf1d4893e8c28752f664fa5ae78c6b
David Tan <chengtan@live.com>
2012-05-23 00:59:06 -0400

    converted the returned result into xml, the brand name is still not being parsed yet

--- medinfo.php
+++ medinfo.php
@@ -1,12 +1,22 @@
 <?php
 	include_once('simple_html_dom.php');
+	function parseToXML($htmlStr)
+{
+$xmlStr=str_replace('<','&lt;',$htmlStr);
+$xmlStr=str_replace('>','&gt;',$xmlStr);
+$xmlStr=str_replace('"','&quot;',$xmlStr);
+$xmlStr=str_replace("'",'&#39;',$xmlStr);
+$xmlStr=str_replace("&",'&amp;',$xmlStr);
+return $xmlStr;
+}
+
 	$drugname=$_GET["drugname"];
 	$url='http://rxnav.nlm.nih.gov/REST/rxcui?name='.$drugname;
 	$xml = simplexml_load_file($url);
 	
 	$mname=$xml->idGroup[0]->name;
 	$drxcui=$xml->idGroup[0]->rxnormId;
-	echo $drxcui.'<br/>';
+	//echo $drxcui.'<br/>';
 
 	$medlineplusurl='http://apps2.nlm.nih.gov/medlineplus/services/mpconnect_service.cfm?mainSearchCriteria.v.cs=2.16.840.1.113883.6.88&mainSearchCriteria.v.c='.$drxcui;
 	
@@ -20,25 +30,32 @@
 		$detailurl=$e->href;
 	
 		$detailhtml=file_get_html($detailurl);
+		header('Content-type: text/xml');
+		echo '<entries>';
 		
 		$titles=$detailhtml->find('h2');
 		
 		foreach($titles as $title){
-			if($title->innertext!='Notice:'){
-			echo '<b>'.$title->innertext.'</b><br/>';
+			if(($title->innertext!='Notice:')&&($title->innertext!='Brand names')){
+			echo '<entry>';
+			echo '<title>'.$title->innertext.'</title>';
+		
 			$titlediv=$title->parent();
+			echo '<detail>'.parseToXML(getblurb($titlediv)).'</detail>';
 			
-			echo getblurb($titlediv);
+			
+			echo '</entry>';
 			}
 		}
 		
+		echo '</entries>';
 	}
 	
 	function getblurb($e){
 		$blurb="";
 		if(($e->next_sibling()->tag=='p')||($e->next_sibling()->tag=='h3')){
 			
-			$blurb.=$e->next_sibling()->innertext.'<br/>';
+			$blurb.=$e->next_sibling()->innertext;
 			
 			getblurb($e->next_sibling());
 			
